2ba2a38868b297ab66357a17de2b6806a1cd5027
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-04-10 16:03:53 -0700

    add

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,7 +3,7 @@ export default function guardrail(mathFunction) {
 
   try {
     queue.push(mathFunction());
-  } catch(err) {
+  } catch (err) {
     queue.push(String(err));
   } finally {
     queue.push('Guardrail was processed');

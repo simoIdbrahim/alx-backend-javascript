c46f0575d5cf79b8f8de2c0300263296df24bb03
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-04-10 14:57:47 -0700

    add

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,3 +1,6 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);

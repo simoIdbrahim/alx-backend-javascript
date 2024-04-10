2cbd8d3e39cb844ffb4c61f8713e92bb6d3ed7db
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-04-10 15:00:10 -0700

    add

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,20 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  return Promise.all([userPromise, photoPromise])
-    .then(([ userData, photoResult ]) => {
-      return [
-        { status: 'fulfilled', value: userData },
-        { status: 'fulfilled', value: photoResult }
-      ];
-    })
-    .catch(error => {
-      return [
-        { status: 'fulfilled', value: error }
-      ];
-    });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => (
+      result.map((req) => ({
+        status: req.status,
+        value: req.status === 'fulfilled' ? req.value : String(req.reason),
+      }))
+    ));
 }

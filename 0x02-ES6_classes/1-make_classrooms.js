fbab5b5e37445340a408b4af082503416ca71e11
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-04-11 05:45:41 -0700

    add

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,7 +3,9 @@ import ClassRoom from './0-classroom.js';
 export default function initializeRooms () {
   const sizes = [19, 20, 34];
   const rooms = [];
-  sizes.forEach((size) => rooms.push(new ClassRoom(size)));
+  for (const size of sizes) {
+    rooms.push(new ClassRoom(size));
+  }
   return rooms;
 }
 

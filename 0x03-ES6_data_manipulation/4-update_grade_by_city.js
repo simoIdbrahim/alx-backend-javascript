5f5776243c17ff664b7449dc59c929f4f4ffb55d
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-04-15 08:30:20 -0700

    add

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students.filter((student) => student.location === city)
+    .map((student) => {
+      const newGrade = newGrades.find(grade => grade.studentId === student.id);
+      return {
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: newGrade ? newGrade.grade : 'N/A',
+      };
+    });
+}

531db33b8dfe34331ddb600c99ff4586050748ae
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-04-16 06:03:58 -0700

    add

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,14 @@
 export default function cleanSet(set, startString) {
   const result = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const val of set.values()) {
+    if (typeof val === 'string' && val.startsWith(startString)) {
+      const str = val.substring(startString.length);
 
-  for (const str of set.values()) {
-    if (str.startsWith(startString)) {
-      const substr = str.substring(startString.length);
-
-      if (substr !== str) {
-        result.push(substr);
+      if (str && str !== val) {
+        result.push(str);
       }
     }
   }

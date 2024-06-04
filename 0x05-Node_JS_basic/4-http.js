a86110006e5407d8f30b0c0cc3697dd812e8238e
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-06-04 15:00:44 +0000

    add

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,10 +4,11 @@ const PORT = 1245;
 const HOST = 'localhost';
 const app = http.createServer();
 
-app.on('request', (req, res) => {
+app.on('request', (_, res) => {
   const responseText = 'Hello Holberton School!';
 
   res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
   res.statusCode = 200;
   res.write(Buffer.from(responseText));
 });

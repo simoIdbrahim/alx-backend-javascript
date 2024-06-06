e92c5c1c9e81e300001fa36a5d696d6e5e1f7c5c
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-06-06 16:12:04 +0000

    fix err

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
   describe('type == "SUM"', () => {
